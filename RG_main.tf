fbfb72647b146529816b213b03ebd78b13cd695b
itzzam <128672336+itzzam@users.noreply.github.com>
2023-08-20 17:55:21 +0530

    Create RG_main.tf
    Deploying Resource group

---
+++ RG_main.tf
@@ -0,0 +1,60 @@
+
+
+/*
+
+
+The following links provide the documentation for the new blocks used
+
+in this terraform configuration file
+
+ 
+
+1. azurerm_resource_group - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+ 
+
+*/
+
+ 
+
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      source = "hashicorp/azurerm"
+
+      version = "3.8.0"
+
+    }
+
+  }
+
+}
+
+ 
+
+provider "azurerm" {
+
+  subscription_id = "81b842ba-4ccd-458f-8210-7d0ea37c841b"
+
+  client_id       = "d386b8da-e7b2-479c-b2eb-d5c23b866dfd"
+
+  client_secret   = "72cf4fb3-f4a1-44c9-9050-bb243c5f3244"
+
+  tenant_id       = "310f7b47-192e-45f1-8dc0-e9639ee6bf8c"
+
+  features {}
+
+}
+
+ 
+
+resource "azurerm_resource_group" "appgrp" {
+
+  name     = "app-grp"
+
+  location = "North Europe"
+
+}
